88cd7a74c036951c7d95754b9d49a40008cf7f06
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2018-11-27 16:10:00 +0100

    complete GET /users test

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,32 @@
 let express = require('express')
 let router = express.Router()
 
+let users = [
+    {
+        id: 1,
+        name: 'Admin',
+        surname: 'Admin',
+        mail: 'admin@admin.com',
+        role: 'teacher'
+    },
+    {
+        id: 2,
+        name: 'Gianmarco',
+        surname: 'Digiacomo',
+        mail: 'giandigia@email.com',
+        role: 'student'
+    }, 
+    {
+        id: 3,
+        name: 'Andrea',
+        surname: 'Rosset',
+        mail: 'trglaw@gmail.com',
+        role: 'student'
+    }
+]
+
 router.get('/users', (req, res, next) => {
-    res.status(500).send('Something broke')
+    res.status(200).json(users)
 })
 
 module.exports = router
--- tests/users.test.js
+++ tests/users.test.js
@@ -5,15 +5,15 @@ const PORT = require('../index').PORT
 let BASE_URL = `http://localhost:${PORT}/api/v1/users` 
 
 test('GET all users', () => {
-    expect.assertions(1)
+    expect.assertions(5)
     return fetch(BASE_URL)
         .then(res => {
-            expect(res.status).toBe(500)
+            expect(res.status).toBe(200)
             return res.json()
         })
         .then(resJson => {
-            expect(resJson).toBeInstanceOf('array')
-            expect(resJson).toHaveLength(465)
+            expect(typeof resJson).toBe('object')
+            expect(resJson).toHaveLength(3)
             let first_user = resJson[0]
             expect(first_user.id).toBe(1)
             expect(first_user.name).toBe('Admin')
