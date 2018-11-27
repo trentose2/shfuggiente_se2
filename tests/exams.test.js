91c72348a4f28c5af4ce3898c00abee5ada62333
Andrea Rosset <trfglaw@gmail.com>
2018-11-27 17:39:12 +0100

    add exam creation without too few informations  and compression of few parameters tests

--- tests/exams.test.js
+++ tests/exams.test.js
@@ -47,7 +47,7 @@ test('Create a valid Exam with exercises and groups', () => {
 
 })
 
-test('Create a valid Exam with exercises', () => {
+test('Create a valid Exam with fewer informations', () => {
 
     const body = {
         author_id: 3,
@@ -82,86 +82,6 @@ test('Create a valid Exam with exercises', () => {
             expect(response_body.name).toBe("Software Engineering II - 15/11/2018")
             expect(response_body.exercises > body.exercises).toBe(false)
             expect(response_body.exercises < body.exercises).toBe(false)
-            expect(response_body.groups.lenght()).toBe(0)
-            expect(response_body.deadline).toBe('2019-07-21T17:32:28Z')
-        })
-
-})
-
-test('Create a valid Exam with groups', () => {
-
-    const body = {
-        author_id: 3,
-        name: "Software Engineering II - 15/11/2018",
-        groups: [13, 15, 17],
-        deadline: '2019-07-21T17:32:28Z'
-    }
-
-    expect.assertions(10)
-    return fetch(BASE_URL, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json'}, 
-                body: JSON.stringify(body)
-        })
-        .then(res => {
-            expect(res.status).toBe(200)
-            return res.json()
-        })
-        .then(response_body => {
-            expect(response_body.id).toBeGreaterThan(0)
-            return response_body.id
-        })
-        .then(id => {
-            return fetch(BASE_URL + '/' + id)
-        })
-        .then(res => {
-            expect(res.status).toBe(200)
-			return res.json()
-        })
-        .then(response_body => {
-            expect(response_body.author_id).toBe(3)
-            expect(response_body.name).toBe("Software Engineering II - 15/11/2018")
-            expect(response_body.exercises.length()).toBe(0)
-            expect(response_body.groups > body.groups).toBe(false)
-            expect(response_body.groups < body.groups).toBe(false)
-            expect(response_body.deadline).toBe('2019-07-21T17:32:28Z')
-        })
-
-})
-
-test('Create a valid Exam without groups and exercises', () => {
-
-    const body = {
-        author_id: 3,
-        name: "Software Engineering II - 15/11/2018",
-        deadline: '2019-07-21T17:32:28Z'
-    }
-
-    expect.assertions(10)
-    return fetch(BASE_URL, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json'}, 
-                body: JSON.stringify(body)
-        })
-        .then(res => {
-            expect(res.status).toBe(200)
-            return res.json()
-        })
-        .then(response_body => {
-            expect(response_body.id).toBeGreaterThan(0)
-            return response_body.id
-        })
-        .then(id => {
-            return fetch(BASE_URL + '/' + id)
-        })
-        .then(res => {
-            expect(res.status).toBe(200)
-			return res.json()
-        })
-        .then(response_body => {
-            expect(response_body.author_id).toBe(3)
-            expect(response_body.name).toBe("Software Engineering II - 15/11/2018")
-            expect(response_body.exercises.lenght()).toBe(0)
             expect(response_body.groups.lenght()).toBe(0)
             expect(response_body.deadline).toBe('2019-07-21T17:32:28Z')
         })
@@ -311,6 +231,24 @@ test('Create exam with invalid (negative) groups', () => {
         name: "Software Engineering II - 15/11/2018",
         groups: [13, -15, 17],
         deadline: '2019-07-21'
+    }
+
+    expect.assertions()
+    return fetch(BASE_URL, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'}, 
+                body: JSON.stringify(body)
+    })
+    .then(res => {
+        expect(res.status).toBe(400) //bad request
+    })
+})
+
+test('Create exam with too few arguments', () => {
+
+    const body = {
+        author_id: 'id',
+        name: "Software Engineering II - 15/11/2018",
     }
 
     expect.assertions()
