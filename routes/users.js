cf2930a70ecd5419505933652a1b9b942fd37ec8
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2018-11-27 18:17:33 +0100

    Add POST tests and implementation

--- routes/users.js
+++ routes/users.js
@@ -29,6 +29,30 @@ router.get('/users', (req, res, next) => {
     res.status(200).json(users)
 })
 
+router.post('/users', (req, res, next) => {
+    let user = req.body
+    if(user.id === undefined) {
+        if(user.name !== undefined && user.surname !== undefined && user.mail !== undefined && user.role !== undefined) {
+            let id = users.length + 1
+            let correctUser = {
+                id: id,
+                name: user.name,
+                surname: user.surname,
+                mail: user.mail,
+                role: user.role
+            }
+            users.push(correctUser)
+            res.status(200).send({
+                id: id
+            })
+        } else {
+            res.status(400).send('Missing parameters in user creation')
+        }
+    } else {
+        res.status(400).send('Forbidden to specify user id during creation')
+    }
+})
+
 router.get('/users/:id', (req, res, next) => {
     let id = parseInt(req.params.id)
     let usersMatching = users.filter(elem => {
--- tests/users.test.js
+++ tests/users.test.js
@@ -45,6 +45,91 @@ test('GET single user with non-existent ID', () => {
         })
 })
 
+test('GET single user with wrong path paramter', () => {
+    expect.assertions(1)
+    return fetch(`${BASE_URL}/giandigia`)
+        .then(res => {
+            expect(res.status).toBe(404)
+        })
+})
+
+test('POST create a new user with correct data', () => {
+    let body = {
+        name: 'Fabio',
+        surname: 'Casagrande',
+        mail: 'febo.casa@gmail.com',
+        role: 'student'
+    }
+
+    expect.assertions(8)
+    return fetch(`${BASE_URL}`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => {
+        expect(res.status).toBe(200)
+        return res.json()
+    })
+    .then(resJson => {
+        let id = resJson.id
+        expect(id).toBeGreaterThan(0)
+        return id
+    })
+    .then(id => {
+        return fetch(`${BASE_URL}/${id}`)
+    })
+    .then(res => {
+        expect(res.status).toBe(200)
+        return res.json()
+    })
+    .then(resJson => {
+        expect(typeof resJson).toBe('object')
+        expect(resJson.name).toBe('Fabio')
+        expect(resJson.surname).toBe('Casagrande')
+        expect(resJson.mail).toBe('febo.casa@gmail.com')
+        expect(resJson.role).toBe('student')
+    })
+})
+
+test('POST create a new user with ID', () => {
+    let body = {
+        id: 5,
+        name: 'Fabio',
+        surname: 'Casagrande',
+        mail: 'febo.casa@gmail.com',
+        role: 'student'
+    }
+
+    expect.assertions(1)
+    return fetch(`${BASE_URL}`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => {
+        expect(res.status).toBe(400)
+    })
+})
+
+test('POST create a new user without required parameters', () => {
+    let body = {
+        name: 'Fabio',
+        surname: 'Casagrande',
+        role: 'student'
+    }
+
+    expect.assertions(1)
+    return fetch(`${BASE_URL}`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => {
+        expect(res.status).toBe(400)
+    })
+})
+
 afterAll(() => {
 	server.close()
 })
