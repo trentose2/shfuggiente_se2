dc4d4148fd7fd0d65d32c8d377ebc0dfff65708e
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2018-11-27 17:17:20 +0100

    complete GET /users/:id tests

--- routes/users.js
+++ routes/users.js
@@ -29,4 +29,16 @@ router.get('/users', (req, res, next) => {
     res.status(200).json(users)
 })
 
+router.get('/users/:id', (req, res, next) => {
+    let id = parseInt(req.params.id)
+    let usersMatching = users.filter(elem => {
+        return elem.id === id
+    })
+    if(usersMatching.length === 1) {
+        res.status(200).send(usersMatching[0])
+    } else {
+        res.status(404).send('404 - Resource not found')
+    }
+})
+
 module.exports = router
--- tests/users.test.js
+++ tests/users.test.js
@@ -20,6 +20,31 @@ test('GET all users', () => {
         })
 })
 
+test('GET single user with valid ID', () => {
+    expect.assertions(7)
+    return fetch(`${BASE_URL}/2`)
+        .then(res => {
+            expect(res.status).toBe(200)
+            return res.json()
+        })
+        .then(resJson => {
+            expect(typeof resJson).toBe('object')
+            expect(resJson.id).toBe(2)
+            expect(resJson.name).toBe('Gianmarco')
+            expect(resJson.surname).toBe('Digiacomo')
+            expect(resJson.mail).toBe('giandigia@email.com')
+            expect(resJson.role).toBe('student')
+        })
+})
+
+test('GET single user with non-existent ID', () => {
+    expect.assertions(1)
+    return fetch(`${BASE_URL}/0`)
+        .then(res => {
+            expect(res.status).toBe(404)
+        })
+})
+
 afterAll(() => {
 	server.close()
 })
