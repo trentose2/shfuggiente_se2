732b62298f1249de36cd4b8978d8a9a228a5e8db
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2018-11-27 18:18:57 +0100

    add body-parser dependency

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3000
 
+app.use(bodyParser.json())
 app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
