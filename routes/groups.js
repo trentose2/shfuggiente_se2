4e1af25b88939a1ca116dadb58ece744c1d368a0
PopBogdan97 <vasile.pop@studenti.unitn.it>
2018-11-27 18:29:46 +0100

    add single group test

--- routes/groups.js
+++ routes/groups.js
@@ -3,15 +3,53 @@ let router = express.Router()
 
 var groups = [{
     "id": 1,
-    "name": "Group1"
+    "name": "Group 1",
+    "members": [
+        1,
+        98,
+        54,
+        78,
+        100
+    ],
+    "exams": [
+        45,
+        12,
+        76,
+        26
+    ]
 },
 {
     "id": 2,
-    "name": "Group2"
+    "name": "Group 2",
+    "members": [
+        4,
+        65,
+        6,
+        23,
+        678
+    ],
+    "exams": [
+        23,
+        54,
+        78,
+        12
+    ]
 }];
 
 router.get('/groups', (req, res, next) => {
     res.status(200).send(groups)
+})
+
+router.get('/groups/:id', (req, res, next) => {
+    let id = parseInt(req.params.id)
+    let groupsMatching = groups.filter(elem => {
+        return elem.id === id
+    })
+    if (groupsMatching.length === 1) {
+        res.status(200).send(groupsMatching[0])
+    } else {
+        res.status(404).send('404 - Resource not found')
+    }
 })
 
 module.exports = router
--- tests/groups.test.js
+++ tests/groups.test.js
@@ -16,10 +16,42 @@ test('GET all groups', () => {
             expect(resJson).toHaveLength(2)
             let first_user = resJson[0]
             expect(first_user.id).toBe(1)
-            expect(first_user.name).toBe('Group1')
+            expect(first_user.name).toBe('Group 1')
             let second_user = resJson[1]
             expect(second_user.id).toBe(2)
-            expect(second_user.name).toBe('Group2')
+            expect(second_user.name).toBe('Group 2')
+        })
+})
+
+
+test('GET single group with valid ID', () => {
+    expect.assertions(6)
+    return fetch(`${BASE_URL}/1`)
+        .then(res => {
+            expect(res.status).toBe(200)
+            return res.json()
+        })
+        .then(resJson => {
+            expect(typeof resJson).toBe('object')
+            expect(resJson.id).toBe(1)
+            expect(resJson.name).toBe('Group 1')
+            expect(resJson.members).toEqual(expect.arrayContaining([1,
+                98,
+                54,
+                78,
+                100]))
+            expect(resJson.exams).toEqual(expect.arrayContaining([45,
+                12,
+                76,
+                26]))
+        })
+})
+
+test('GET single group with non-existent ID', () => {
+    expect.assertions(1)
+    return fetch(`${BASE_URL}/0`)
+        .then(res => {
+            expect(res.status).toBe(404)
         })
 })
 
