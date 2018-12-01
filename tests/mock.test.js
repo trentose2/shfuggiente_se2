7704bde03220979ab97b099a13db37775a341ed8
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2018-12-01 22:46:12 +0100

    remove unused variables

--- tests/mock.test.js
+++ tests/mock.test.js
@@ -1,9 +1,5 @@
-const fetch = require('node-fetch')
-const PORT = process.env.PORT || 3000
 const app = require('../app')
 const request = require('supertest')
-
-let BASE_URL = `http://localhost:${PORT}/` 
 
 describe('Test the root path', () => {
     test('It should response the GET method', () => {
