0582adb88ae7d348b879fe1ca4e2800905c2ce2e
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2018-12-01 15:56:16 +0100

    refactor to use supertest

--- tests/mock.test.js
+++ tests/mock.test.js
@@ -1,16 +1,16 @@
 const fetch = require('node-fetch')
 const PORT = process.env.PORT || 3000
+const app = require('../app')
+const request = require('supertest')
 
 let BASE_URL = `http://localhost:${PORT}/` 
 
-test('hello world', () => {
-	expect.assertions(2)
-	return fetch(BASE_URL)
-		.then(res => {
-			expect(res.status).toBe(200)
-			return res.text()
-		})
-		.then(text => {
-			expect(text).toBe('Hello World!')
-		})
+describe('Test the root path', () => {
+    test('It should response the GET method', () => {
+		return request(app)
+			.get('/api/v1')
+			.then((res) => {
+            	expect(res.statusCode).toBe(200)
+        	})
+    })
 })
